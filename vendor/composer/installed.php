701ded3ab49fa8c7a5215cedc31733d73a825862
gelvazio <gelvazio@tidas.com.br>
2022-12-14 23:05:12 -0300

    Changes

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9c993314b7bc90e3fb2fdc883578fd2df4558551',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9c993314b7bc90e3fb2fdc883578fd2df4558551',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'heroku/heroku-buildpack-php' => array(
+            'pretty_version' => 'v227',
+            'version' => '227.0.0.0',
+            'reference' => '29337531e7db820ba6f4d45a9d6521f60b8c27d9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../heroku/heroku-buildpack-php',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
